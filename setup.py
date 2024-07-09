041b0d5cff1ef803a81ddacb12e6624ae584b58b
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-07-09 15:02:44 +0000

    setup setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
 
+setup(
+  name = 'InsightAsiaDataProcessor',
+  version = '0.1',
+  author = 'Ha Hoang Hao',
+  packages = setup.find_packages(),
+  description = 'Auto Generate SPSS syntax',
+  install_requires = ['re', 'requests', 'bs4', 'pandas']
+)
