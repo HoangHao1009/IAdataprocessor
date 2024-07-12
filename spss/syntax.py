810afda2e1d0b017a4f7503aebbd3fc09fcdbc4d
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-07-12 05:03:33 +0000

    fix compute syntax

--- spss/syntax.py
+++ spss/syntax.py
@@ -91,9 +91,8 @@ IF ({question} = 6 OR {question} = 7) {new_question} = 2.
 IF ({question} = 8 OR {question} = 9 OR {question} = 10) {new_question} = 3.
     '''
             value_label_command = value_label(new_question, {1: 'Bottom 5 boxes', 2: 'Neutral', 3: 'Top 3 boxes'})
-        command = if_command, value_label_command
-
-        return command
+            
+        return if_command, value_label_command
 
     if_command, value_label_command = take_command(type)
 
