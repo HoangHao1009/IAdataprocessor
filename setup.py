fea5c5cba1b124991a4e75ad506e9766baf875fc
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-07-12 04:11:36 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'InsightAsiaDataProcessor',
+  name = 'ia',
   version = '0.1',
   author = 'Ha Hoang Hao',
   packages = find_packages(),
