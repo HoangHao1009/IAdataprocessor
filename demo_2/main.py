9a35fd374f6e763212ba71f10c0cb7640b8ad75b
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-07-31 07:22:18 +0000

    create Survey

--- demo_2/main.py
+++ demo_2/main.py
@@ -1,3 +1,5 @@
+import os
+
 from dataclasses import dataclass
 from question import Questionnaire
 from response import Responsers
@@ -16,3 +18,14 @@ class Survey:
     def __init__(self, config):
         self.questionaire = Questionnaire(config)
         self.response = Responsers(config)
+
+    def get_SPSS(self):
+        return Questionnaire.create_syntax()
+    
+    def get_responseDf(self):
+        return Responsers.dfs
+    
+    def get_sql(self, folder_path):
+        qa = Questionnaire.qa.to_csv(os.path.join(folder_path, 'qa.csv'))
+        resp_info = Responsers.dfs['resp_info'].to_csv(os.path.join(folder_path, 'resp_info.csv'))
+        resp_answers = Responsers.dfs['resp_answers'].to_csv(os.path.join(folder_path, 'resp_answers.csv'))
