654e655e9d8ec0a8ab3019156aacd1ffd1f3ae7a
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-08-06 02:40:46 +0000

    .

--- oe/main.py
+++ oe/main.py
@@ -14,11 +14,13 @@ class OE:
         })
     
     def process_OE(self, question_code, function, answer_value=True):
-        info_df = self.Fact.query('questionCode == @question_code')
+        info_df = self.Fact.query('questionCode == @question_code').copy()
         if answer_value:
             info_df['processed'] = info_df['answerValuesText'].apply(function)
         else:
             info_df['processed'] = info_df['answerText'].apply(function)
         info_df = info_df.explode('processed')
         info_df.rename(columns={'answerValuesText': 'root', 'answerText': 'root'}, inplace=True)
+        print(self.dataframes.columns)
+        print(info_df.columns)
         self.dataframes = pd.concat([self.dataframes, info_df])
